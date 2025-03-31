48e8d2e408264d676f3f0cce71c3cf133d4aea08
sskkyy0 <zxzxzxzxzxzxzz@naver.com>
2025-03-31 18:17:48 +0900

    feat : 플러스 버튼 라운드 값 구현

--- app/src/main/java/com/example/androidanimalproject/animal/SearchScreen.kt
+++ app/src/main/java/com/example/androidanimalproject/animal/SearchScreen.kt
@@ -9,22 +9,19 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kotlin.math.round
 
 @Composable
 fun AnimalComponent() {
@@ -33,34 +30,37 @@ fun AnimalComponent() {
 
 @Composable
 fun SearchScreen() {
-    val Corange=Color(0xFFFFA938)
+    val orangeColor = Color(0xFFFFA938)
     Column {
         Box(
             modifier = Modifier
                 .height(56.dp)
                 .fillMaxWidth(),
             contentAlignment = Alignment.Center
-        ){
+        ) {
             Text("조회하기")
         }
         LazyColumn(
             modifier = Modifier.align(Alignment.CenterHorizontally)
         ) {
-            items(5){ index ->
-                Box(){
+            items(5) { index ->
+                Box() {
                     Text("index")
                 }
             }
         }
     }
     Box(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
             .padding(20.dp),
         contentAlignment = Alignment.BottomEnd
-    ){
+    ) {
         IconButton(
-            modifier = Modifier.size(56.dp)
-                .background(color = Corange),
+            modifier = Modifier
+                .size(56.dp)
+                .clip(RoundedCornerShape(20.dp))
+                .background(color = orangeColor), // modifier 순서에따라 달라짐
             onClick = {}) {
             Icon(
                 imageVector = Icons.Filled.Add,
@@ -75,5 +75,5 @@ fun SearchScreen() {
 @Preview
 @Composable
 private fun SearchscreenPrev() {
-   SearchScreen()
+    SearchScreen()
 }
